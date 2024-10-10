39c68f9bb59d4321d92ac7ec9cc775dbe7287e32
Gadoskey <gadoski321@gmail.com>
2024-10-11 00:36:29 +0100

    solved task 0

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Simple Addition Function
+
+Author: Yusuf Mustapha Opeyemi
+
+This function takes two floating-point numbers as input and returns their sum.
+
+Parameters:
+  num1: The first floating-point number.
+  num2: The second floating-point number.
+
+Returns: The sum of `num1` and `num2`.
+"""
+
+def add(num1: float, num2: float) -> float:
+    # Adds num1 and num2 together and Returns the result of the addition
+    return num1 + num2
