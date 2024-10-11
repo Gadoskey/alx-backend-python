7dcd6c359c3fac4f59265cc8a8c01294df0c1645
Gadoskey <gadoski321@gmail.com>
2024-10-11 09:02:08 +0100

    done

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -16,6 +16,6 @@
 
 def concat(str1: str, str2: str) -> str:
     '''
-      Adds num1 and num2 together and returns the result of the addition
+      Adds str1 and str2 together and returns the result of the addition
     '''
     return str1 + str2
