6b558a2aecd0864c9706b7c718700dc1fb4eb441
Gadoskey <gadoski321@gmail.com>
2024-10-11 08:40:44 +0100

    kkk

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-"""
-Simple Addition Function
 
-Author: Yusuf Mustapha Opeyemi
-
-This function takes two floats as input and returns their sum.
-
-Parameters:
-  num1: The first floating-point number.
-  num2: The second floating-point number.
-
-Returns: The sum of `num1` and `num2`.
-"""
+'''
+    Basic annotations for variables.
+'''
 
 
-def add(num1: float, num2: float) -> float:
-    # Adds num1 and num2 together and returns the result of the addition
-    return num1 + num2
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
+    return a + b
