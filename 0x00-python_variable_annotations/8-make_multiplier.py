b9ce7f95de8d14990c6e8b35ebd5117b4616c664
Gadoskey <gadoski321@gmail.com>
2024-10-11 12:24:18 +0100

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+  Complex types - functions
+  Author: Yusuf Mustapha Opeyemi
+'''
+from typing import Callable, Iterator, Union, Optional, List, Tuple
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+       Function make_multiplier that takes a float multiplier as argument.
+       Returns a function that multiplies a float by multiplier.
+    '''
+    def mul(number: float) -> float:
+      ''' Mulitiplies a float by multipler '''
+      return float(number * multiplier)
+    
+    return mul
