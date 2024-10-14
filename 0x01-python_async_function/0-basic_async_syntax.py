9f3c2865ba64f3b35ce62044e694b3b91d75d609
Gadoskey <gadoski321@gmail.com>
2024-10-14 17:01:58 +0100

    avoid multiple imports in one line

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,12 +6,14 @@
   Author: Yusuf Mustapha Opeyemi
 '''
 
-import asyncio, random
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     '''
-      An asynchronous coroutine named wait_random that waits for a random delay between 0 and max_delay seconds and eventually returns it
+      An asynchronous coroutine named wait_random that waits for 
+        a random delay between 0 and max_delay seconds and eventually returns it
     '''
     randomDelay = random.uniform(0, max_delay)
     await asyncio.sleep(randomDelay)
