51c93622959c7cb82aab7802c53a77894eea8043
Gadoskey <gadoski321@gmail.com>
2024-10-14 17:35:05 +0100

    solved task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,7 +16,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
-    Asynchronous coroutine that spawns wait_random n times with the specified max_delay.
+    Asynchronous coroutine that spawns wait_random n times
+    with the specified max_delay.
 
     Args:
         n (int): The number of times to call wait_random.
@@ -27,7 +28,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     # Create a list of coroutines
     coroutines = [wait_random(max_delay) for i in range(n)]
-    
+
     # Gather results of all coroutines
     results = await asyncio.gather(*coroutines)
 
