76ba08cb0a92ee271a1abaf86c4cd868b026910a
Gadoskey <gadoski321@gmail.com>
2024-11-06 18:05:43 +0100

    removed whitespaces

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,7 +22,7 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test that GithubOrgClient.org returns the correct value."""
         # Set up the mock return value for get_json
         expected_response = {"name": org_name, "repos_url"
-                             : f"https://api.github.com/orgs/{org_name}/repos"}
+                            : f"https://api.github.com/orgs/{org_name}/repos"}
         mock_get_json.return_value = expected_response
 
         # Initialize GithubOrgClient and call the org method
@@ -30,5 +30,6 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client.org
 
         # Assertions
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+          f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected_response)
